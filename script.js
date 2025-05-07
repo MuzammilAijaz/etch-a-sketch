e137326cd153b3cc8a694c818d622b6ae2429f1d
Muzammil <171587778+MuzammilAijaz@users.noreply.github.com>
2025-05-07 06:56:26 +0500

    generate 16*16 boxes

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+// Create 16x16 grid of square divs
+let gridContainer = document.querySelector('#gridContainer');
+for(let i = 0; i < 16*16; i++){
+    let grid = document.createElement('div');
+    grid.textContent = 'box';
+    grid.style.backgroundColor = 'grey';
+    grid.style.border = 'black 2px solid';
+    grid.style.margin = '0px';
+    gridContainer.appendChild(grid);
+}
+
